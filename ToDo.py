cd04eb77b0ba4e1bb013a2097a654a2173a6209f
JoaoPedroLeite <jplff18@hotmail.com>
2023-10-04 14:05:19 -0300

    implementacao do raise no erro de conexao nao feita, demais modificacoes concluidas

--- ToDo.py
+++ ToDo.py
@@ -8,30 +8,25 @@ import psycopg2
 
 try:
     conn = psycopg2.connect("dbname='meubanco' user='joao' host='localhost' port='5432' password='1234'")
-    # conn = psycopg2.connect("dbname='meubanco' user='postgres' host='localhost' password='postgres'")
-except:
-    print("I am unable to connect to the database")
+except ConnectionFailure as error:
+    print(f"Erro de conexão: {error}")
 
 
 def listar_tarefas(conn):
-    # we use a context manager to scope the cursor session
     with conn.cursor() as curs:
-
+        curs.execute("SELECT * FROM tarefas;")
         try:
-            curs.execute("SELECT * FROM tarefas;")
-
-            # a default install should include this query and some backend workers
             many_rows = curs.fetchmany(3)
-
-            # use the * unpack operator to print many_rows which is a Python list
-            # print(*many_rows, sep = "\n")
-            print(many_rows)
-            print(type(many_rows))
-            return many_rows
-
-        # a more robust way of handling errors
         except (Exception, psycopg2.DatabaseError) as error:
-            print(error)
+            # print("O erro é:")
+            # print(error)
+            raise NameError("erro de banco desligado")
+        return many_rows
+
+        # except psycopg2.OperationalError as error: 
+        #     # Tratamento do erro de conexão
+        #     print("Erro de conexão com o banco de dados:", error)
+        # return many_rows
 
 
 # def adicionar_tarefa(tarefa):
@@ -80,70 +75,32 @@ def listar_tarefas(conn):
 #     txt.write("\n".join(lista))
 #     txt.close()
 
-class ThingsResource:
+class ListResource:
     def on_get(self, request, response):
-        """Handles GET requests"""
         response.status = falcon.HTTP_200 # This is the default status
-        # response.content_type = falcon.MEDIA_TEXT  # Default is JSON, so override
         lista = listar_tarefas(conn)
-        print(lista)
-        print(type(lista))
-        # response.body = str(lista)
-        # lista_string = listar_tarefas()
         lista_dicionario = {}
         for indice, tarefa in lista:
             lista_dicionario[indice] = tarefa 
         
-        # response.media = lista_dicionario
-
-
-        # response.status = falcon.HTTP_200
-        
-        # response.content_type = falcon.MEDIA_TEXT
-
-        # lista = listar_tarefas(conn)
-
-        # Crie uma lista de strings formatadas
-        # lista_formatada = str(f"{indice}: {tarefa}" for indice, tarefa in lista)
-        # dicionario_string = '\n'.join([f'{chave}: "{valor}"' for chave, valor in lista_dicionario.items()])
-
-
-
-        # print(dicionario_string)
-
-        # Defina a resposta como a lista formatada
-        # response.media = dicionario_string
-
-        #Esse for só deixa o ultimo valor:
-
-        for chave, valor in lista_dicionario.items():
-            response.media = (f'Chave: {chave}, Valor: {valor}')
-            print(f'Chave: {chave}, Valor: {valor}')
+        response.media = lista_dicionario
 
         
     def on_post(self, request, response):
         response.status = falcon.HTTP_200 # This is the default status
         # response.content_type = falcon.MEDIA_TEXT  # Default is JSON, so override
         lista = ler_arquivo()
-        
         body = request.bounded_stream.read()
         dicionario = json.loads(body.decode('utf-8'))
-        # print(dicionario)
-        # print(type(dicionario))
         response.media = {"mensagem": f"recebido a tarefa {dicionario['data']}"}
-
         adicionar_tarefa(dicionario['data'])
 
    
 
-class CowsResource:
+class CreateResource:
     def on_delete(self, request, response, task_id):
         response.status = falcon.HTTP_204
         response.content_type = falcon.MEDIA_TEXT  # Default is JSON, so override.status
-        print(dir(request))
-        print(task_id)
-        print(type(task_id))
-        # response.text = "ok"
         erro = remover_tarefa(task_id)
 
         if erro == False:
@@ -168,15 +125,9 @@ class CowsResource:
         atualizar_tarfa(task_id,dicionario['data'])
 
 
-
-
-
 app = falcon.App()
-
-
-things = ThingsResource()
-things_cows = CowsResource()
-
+things = ListResource()
+things_cows = CreateResource()
 
 app.add_route('/tarefas/', things)
 app.add_route('/tarefas/{task_id:int}', things_cows)
